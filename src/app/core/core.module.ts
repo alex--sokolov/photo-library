8052669da2c8b5ecdb7dc6dd130eb888acced788
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-09-26 18:16:34 +0300

    init: start project

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule {
+}
