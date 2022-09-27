f3868e9bb787fdaae124341e25acd53ab2de1ad7
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-09-28 02:29:51 +0300

    fix: fix environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
     url: 'https://picsum.photos/200/300',
     delay: [200, 300],
     limit: 6,
+    urlPicsum: ['https://i.picsum.photos/id/', '/200/300.jpg?hmac=']
   },
 };
